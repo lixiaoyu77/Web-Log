d27cbbb144a24bb8af934861d0bf616ea6c5f2f3
leo <82462733+lixiaoyu77@users.noreply.github.com>
2024-01-19 14:15:28 +0800

    Create 防抖.js

---
+++ 手撕代码/防抖.js
@@ -0,0 +1,22 @@
+function debounce(func, wait) {
+    let timeout;
+
+    return function () {
+        let context = this; // 保存this指向
+        let args = arguments; // 拿到event对象
+
+        clearTimeout(timeout)
+        timeout = setTimeout(function(){
+            func.apply(context, args)
+        }, wait);
+    }
+}
+
+// 创建一个防抖版本的函数
+const debouncedFunction = debounce(function() {
+    console.log("Function is debounced");
+}, 200);
+
+// 调用防抖函数
+debouncedFunction();
+debouncedFunction(); // 在200毫秒内再次调用，将会重新计时
