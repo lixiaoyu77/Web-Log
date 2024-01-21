dbc294b4bcaf9dd43d8b937b4e93c98484480b01
leo <82462733+lixiaoyu77@users.noreply.github.com>
2024-01-21 19:58:51 +0800

    update

---
+++ 手撕代码/异步实现加减法.js
@@ -0,0 +1,29 @@
+function addAsync(a, b) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const result = a + b;
+        resolve(result);
+      }, 1000); // 模拟异步操作，比如网络请求或定时任务
+    });
+  }
+
+// 使用异步加法
+addAsync(2, 3)
+.then(result => {
+  console.log(result); // 输出 5
+})
+.catch(error => {
+  console.error(error);
+});
+
+async function performAddition() {
+    try {
+      const result = await addAsync(2, 3);
+      console.log(result); // 输出 5
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  // 调用使用 async/await 处理的异步加法
+  performAddition();
