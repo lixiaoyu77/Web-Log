a2a42137f0509fb01bf765a3d7e5eb91f9f72203
leo <82462733+lixiaoyu77@users.noreply.github.com>
2024-01-22 15:26:25 +0800

    update

---
+++ 手撕代码/深拷贝.js
@@ -0,0 +1,26 @@
+function deepCopy(obj, memo = new WeakMap()) {
+    // 处理对象
+    const copyObj = {};
+    memo.set(obj, copyObj); // 将原对象和复制对象关联起来，防止循环引用
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            copyObj[key] = deepCopy(obj[key], memo);
+        }
+    }
+
+    return copyObj;
+}
+
+// 示例
+const originalObject = {
+    name: 'John',
+    age: 25,
+    address: {
+        city: 'New York',
+        zip: '10001'
+    },
+    hobbies: ['reading', 'traveling']
+};
+
+const clonedObject = deepCopy(originalObject);
+console.log(clonedObject);
