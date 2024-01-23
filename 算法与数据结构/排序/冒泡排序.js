e73fececaf650054c64c41cc5a7cd6b9afa2d2ba
leo <82462733+lixiaoyu77@users.noreply.github.com>
2024-01-23 14:22:02 +0800

    Update 冒泡排序.js

---
+++ 算法与数据结构/排序/冒泡排序.js
@@ -0,0 +1,17 @@
+// 冒泡排序
+// 重复地遍历要排序的列表，一次比较两个元素
+const a1 = [1,4,7,2,12,6]
+
+function sort(arr){
+    let len = arr.length
+    for(let i=0;i< len-1;i++){
+        for(let j=0;j<len-1;j++){
+            if(arr[j]>arr[j+1])
+            [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
+        }
+    }
+    return arr;
+}
+
+const bubblesort = sort(a1)
+console.log(bubblesort)
