3aeb820d0e7e65385ed21fd3c53fc61a3bda1b41
leo <82462733+lixiaoyu77@users.noreply.github.com>
2024-01-23 11:28:50 +0800

    Update 合并有序数组.js

--- 手撕代码/合并有序数组.js
+++ 手撕代码/合并有序数组.js
@@ -1,16 +1,39 @@
-const arr1 = [1,4,5,9]
-const arr2 = [1,3,7,9,14]
+const a1 = [1,4,5,9]
+const a2 = [1,3,7,9,14]
 
 // 思路: 
 // 双指针法
 // 比较两个数组的元素，将较小的元素添加到合并数组中，然后移动指针。
 //最后，如果其中一个数组还有剩余元素，将其全部添加到合并数组的末尾
 function mergeSort(arr1,arr2){
-    let mergeArr;
+    const mergeArr = [];
+    let i = 0;
+    let j = 0;
+    
+    while(i<arr1.length && j<arr2.length){
+        if(arr1[i]<arr2[j]){
+            mergeArr.push(arr1[i]);
+            i++;
+        }else{
+            mergeArr.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while(i<arr1.length){
+        mergeArr.push(arr1[i]);
+        i++;
+    }
+
+    while(j<arr2.length){
+        mergeArr.push(arr2[j]);
+        j++;
+    }
+    return mergeArr;
 }
 
 
-
-
+const arr = mergeSort(a1,a2);
+console.log(arr)
 
 // 使用方法 concat/sort
