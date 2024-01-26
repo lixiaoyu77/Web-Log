86f96d2d7782441e54c9ea3d6c0e55f7da36e4f6
leo <82462733+lixiaoyu77@users.noreply.github.com>
2024-01-26 11:22:38 +0800

    Update 节流.js

--- 手撕代码/节流.js
+++ 手撕代码/节流.js
@@ -14,28 +14,3 @@ const throttledFn = throttled(function (a, b, c) {
 }, 500);
 
 throttledFn(1, 2, 3);
-
-
-
-function throttled(fn,delay=500){
-    let oldtime = Date.now()
-    return function(...args){
-        newtime = Date.now()
-        if(newtime - oldtime > delay){
-            fn.apply(null,args)
-            oldtime = Date.now();
-        }
-    }
-}
-
-function debounce(fn,wait){
-    let timeout;
-    return function(){
-        let context = this;
-        let args = arguments;
-        clearTimeout(timeout);
-        timeout = setTimeout(function(){
-            fn.apply(context,args);
-        },wait)
-    }
-}
