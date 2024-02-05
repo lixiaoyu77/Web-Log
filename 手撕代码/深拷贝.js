d9f1c9f27e3798bff47164b8e7919760ef338b22
leo <1254885131@qq.com>
2024-02-05 21:02:33 +0800

    Update 深拷贝.js

--- 手撕代码/深拷贝.js
+++ 手撕代码/深拷贝.js
@@ -1,18 +1,19 @@
-function deepCopy(obj, memo = new WeakMap()) {
+function deepCopy (obj, memo = new WeakMap()) {
     // 处理对象
-    const copyObj = {};
-    memo.set(obj, copyObj); // 将原对象和复制对象关联起来，防止循环引用
+    const copyObj = {}
+    memo.set(obj, copyObj) // 将原对象和复制对象关联起来，防止循环引用
     for (let key in obj) {
-        if (obj.hasOwnProperty(key)) {
+        //hasOwnProperty 是 JavaScript 中的方法，用于检查对象是否包含指定属性 
+        if (obj.hasOwnProperty(key)) { //确保只处理对象自身的属性，而不包括从原型链继承的属性
             if (typeof obj[key] === 'object') {
-                copyObj[key] = deepCopy(obj[key], memo); // 递归
+                copyObj[key] = deepCopy(obj[key], memo) // 递归
             } else {
-                copyObj[key] = obj[key];
+                copyObj[key] = obj[key]
             }
         }
     }
 
-    return copyObj;
+    return copyObj
 }
 
 // 示例
@@ -24,7 +25,7 @@ const originalObject = {
         zip: '10001'
     },
     hobbies: ['reading', 'traveling']
-};
+}
 
-const clonedObject = deepCopy(originalObject);
-console.log(clonedObject);
+const clonedObject = deepCopy(originalObject)
+console.log(clonedObject)
