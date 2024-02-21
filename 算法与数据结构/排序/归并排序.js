841bee448f78476fedb23f16c4f2fef2998ec665
leo <1254885131@qq.com>
2024-02-21 17:12:05 +0800

    排序算法

--- 算法与数据结构/排序/归并排序.js
+++ 算法与数据结构/排序/归并排序.js
@@ -1,35 +1,37 @@
-function mergeSort(arr) {
+// 归并排序如何实现，时间复杂度是多少， 还可以如何改进？
+
+function mergeSort (arr) {
     if (arr.length <= 1) {
-        return arr; // 数组长度为1或为空时，无需排序
+        return arr // 数组长度为1或为空时，无需排序
     }
 
-    const mid = Math.floor(arr.length / 2);
-    const left = arr.slice(0, mid);
-    const right = arr.slice(mid);
+    const mid = Math.floor(arr.length / 2)
+    const left = arr.slice(0, mid)
+    const right = arr.slice(mid)
 
-    return merge(mergeSort(left), mergeSort(right));
+    return merge(mergeSort(left), mergeSort(right))
 }
 
-function merge(left, right) {
-    let result = [];
-    let leftIndex = 0;
-    let rightIndex = 0;
+function merge (left, right) {
+    let result = []
+    let leftIndex = 0
+    let rightIndex = 0
 
     while (leftIndex < left.length && rightIndex < right.length) {
         if (left[leftIndex] < right[rightIndex]) {
-            result.push(left[leftIndex]);
-            leftIndex++;
+            result.push(left[leftIndex])
+            leftIndex++
         } else {
-            result.push(right[rightIndex]);
-            rightIndex++;
+            result.push(right[rightIndex])
+            rightIndex++
         }
     }
 
     // 将剩余元素加入结果数组
-    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
 }
 
 // 示例用法
-const unsortedArray = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
-const sortedArray = mergeSort(unsortedArray);
-console.log(sortedArray);
+const unsortedArray = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+const sortedArray = mergeSort(unsortedArray)
+console.log(sortedArray)
