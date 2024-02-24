556c5b1617452dd256c0f361e318665349cb3cc2
leo <1254885131@qq.com>
2024-02-24 21:24:17 +0800

    Update 插入排序.js

--- 算法与数据结构/排序/插入排序.js
+++ 算法与数据结构/排序/插入排序.js
@@ -1,7 +1,20 @@
 // 插入排序如何实现，时间复杂度是多少， 还可以如何改进？
 
-
 // 将数据按照一定的顺序一个一个的插入到有序的表中，最终得到的序列就是已经排序好的数据
 
 
 // 插入排序
+function insertionSort (arr) {
+  const len = arr.length
+  let preIndex, current
+  for (let i = 1; i < len; i++) {
+    preIndex = i - 1
+    current = arr[i]
+    while (preIndex >= 0 && arr[preIndex] > current) {
+      arr[preIndex + 1] = arr[preIndex]
+      preIndex--
+    }
+    arr[preIndex + 1] = current
+  }
+  return arr
+}
