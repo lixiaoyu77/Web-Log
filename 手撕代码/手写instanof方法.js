a60fe0b17d17cc87a96c5bbe016c1d49014c6097
leo <1254885131@qq.com>
2024-03-17 14:04:01 +0800

    Update 手写instanof方法.js

--- 手撕代码/手写instanof方法.js
+++ 手撕代码/手写instanof方法.js
@@ -1,29 +1,4 @@
-function myInstanceOf (obj, constructor) {
-  // 首先判断 constructor 是否为函数
-  if (typeof constructor !== 'function') {
-    throw new TypeError('Right-hand side of instanceof is not callable')
-  }
-
-  // 判断 obj 是否为对象
-  if (obj === null || typeof obj !== 'object') {
-    return false
-  }
-
-  // 获取 constructor 函数的原型对象
-  let proto = Object.getPrototypeOf(obj)
-
-  // 在原型链中循环查找 constructor 函数的原型
-  while (proto !== null) {
-    if (proto === constructor.prototype) {
-      return true
-    }
-    proto = Object.getPrototypeOf(proto)
-  }
-
-  return false
-}
-
-
+// obj, constructor
 function myInstanceof (left, right) {
   // 这里先用typeof来判断基础数据类型，如果是，直接返回false
   if (typeof left !== 'object' || left === null) return false
