19e7c7b0ddc47438da761f4151b72f58df5d4655
leo <1254885131@qq.com>
2024-03-18 14:28:48 +0800

    Update 节流.js

--- 手撕代码/节流.js
+++ 手撕代码/节流.js
@@ -1,16 +1,5 @@
-function throttled (fn, delay = 500) {
-    let oldtime = Date.now()
-    return function (...args) {
-        let newtime = Date.now()
-        if (newtime - oldtime >= delay) {
-            fn.apply(null, args)
-            oldtime = Date.now()
-        }
-    }
-}
-
 // 定时器写法
-function throttled2 (fn, delay = 500) {
+function throttled (fn, delay = 500) {
     let timer = null
     return function (...args) {
         if (!timer) {
